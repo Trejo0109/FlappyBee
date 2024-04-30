744565008fe253b264979ccd6645aaf2f0e21d9f
Rodrigo Hernandez Trejo <rodrigohernandeztrejo1@gmail.com>
2024-04-29 18:00:40 -0600

    ok

---
+++ Escenas/CharacterBody3D.gd
@@ -0,0 +1,28 @@
+extends CharacterBody3D
+
+
+const SPEED = 3
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+	else:
+		velocity.x = 0
+	move_and_slide()
